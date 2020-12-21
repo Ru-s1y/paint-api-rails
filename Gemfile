c9f9d86cd1225d51d6c3838a79405189639350b3
Masashi <optacos@gmail.com>
2020-12-21 19:48:41 +0900

    AWS S3 setting

--- Gemfile
+++ Gemfile
@@ -30,6 +30,9 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'rack-cors'
 gem 'jwt'
 
+# AWS
+gem 'aws-sdk', '~> 3.0', '>= 3.0.1'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/api/v1/pictures_controller.rb
+++ app/controllers/api/v1/pictures_controller.rb
@@ -13,9 +13,28 @@ module Api
       def create
         image = picture_params[:image]
         uri = URI.parse(image)
-        if uri.schema == "data" then
+        if uri.scheme == "data" then
           data = decode(uri)
           extension = extension(uri)
+          imageURL = put_s3 data, extension
+          # if Rails.env.production?
+          # else
+          #   # development or test upload file path
+          #   # data, extension
+          # end
+        end
+        
+        @picture = Picture.new(
+          name: picture_params[:name],
+          description: picture_params[:description],
+          image: imageURL,
+          publish: picture_params[:publish],
+          user_id: current_user.id
+        )
+        if @picture.save
+          render json: @picture
+        else
+          render json: @picture.errors
         end
       end
 
@@ -40,6 +59,43 @@ module Api
         def set_picture
           @picture = Picture.find_by(id: params[:id])
         end
-    end
+
+        # 以下アップロード関連
+
+        # Base64デコード
+        def decode(uri)
+          opaque = uri.opaque
+          data = opaque[opaque.index(",") + 1, opaque.size]
+          Base64.decode64(data)
+        end
+
+        # 拡張子の取得
+        def extension(uri)
+          opaque = uri.opaque
+          mime_type = opaque[0, opaque.index(";")]
+          case mime_type
+          when "image/png" then
+            ".png"
+          when "image/jpeg" then
+            ".jpg"
+          else
+            raise "Unsupport Content-Type"
+          end
+        end
+
+        # AWS S3 Upload
+        def put_s3(data, extension)
+          file_name = Digest::SHA1.hexdigest(data) + extension
+          s3 = Aws::S3::Resource.new(
+            :region => ENV['AWS_S3_REGION'],
+            :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+            :secret_access_key => ENV['AWS_ACCESS_SECRET_KEY']
+          )
+          bucket = s3.bucket(ENV['AWS_S3_BUCKET'])
+          obj = bucket.object("uploader/#{current_user.id}/#{file_name}")
+          obj.put(acl: "public-read", body: data)
+          obj.public_url
+        end
+      end
   end
 end
