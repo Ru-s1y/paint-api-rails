d92295d9ae645c9986fdcf4b4eb8d1ad0c3866ac
Masashi <optacos@gmail.com>
2020-12-21 19:48:41 +0900

    AWS S3 setting

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,4 +3,16 @@ class Picture < ApplicationRecord
   belongs_to :user, optional: true
   validates :name, :image, :publish, :user_id, presence: true
 
+  # development, test環境用
+  # def parse_base64(data, extension)
+  #   filename = Time.zone.now.to_s + extension
+  #   File.open("#{Rails.root}/tmp/#{filename}", 'wb') do |f|
+  #     f.write(data)
+  #   end
+  # end
+
+  private
+    # def attach_image(filename)
+    # end
+
 end
